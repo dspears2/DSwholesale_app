53851fd8bda9d6b5de2a4f4aa175ef86aca31151
DeMarco Spears <demarcospears22@gmail.com>
2023-05-04 12:32:21 -0500

    2nd commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount SolidusPaypalCommercePlatform::Engine, at: '/solidus_paypal_commerce_platform'
   root to: 'home#index'
 
+
   devise_for(:user, {
     class_name: 'Spree::User',
     singular: :spree_user,
